e65101c4381e1307f43786b93b2f4c11a87e5fea
Paulo <ppauloces27@gmail.com>
2022-10-02 20:12:42 -0300

    teste

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!doctype html>
 	<html lang="pt-br">
 	<head>
-		<title>IForúm - Login</title>
+		<title>IForúm - teste</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
